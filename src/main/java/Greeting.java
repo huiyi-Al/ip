01e0d9121818565d7e6d8ed457bfa162c8996cc9
Ziyue <e1295583@u.nus.edu>
2025-10-01 22:46:17 +0800

    Add support for list

--- src/main/java/Greeting.java
+++ src/main/java/Greeting.java
@@ -1,22 +1,30 @@
 package main.java;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Greeting {
+    private static Scanner sc = new Scanner(System.in);
+    private static ArrayList<String> tasks = new ArrayList<String>();
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         greet();
+
         boolean isRunning = true;
         while (isRunning) {
             String instruction = sc.nextLine();
+
             switch (instruction) {
                 case "bye":
                     isRunning = false;
                     break;
-                    default:
-                        System.out.println(instruction);
+                case "list":
+                    listTask();
+                    break;
+                default:
+                    addTask(instruction);
             }
         }
         exit();
+        sc.close();
     }
     public static void greet(){
         System.out.println("Hello! I'm Luka\n" +
@@ -24,5 +32,14 @@ public class Greeting {
     }
     public static void exit(){
         System.out.println("Bye.Hope to see you again soon!");
+    }
+    public static void listTask(){
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i + 1) + "." + tasks.get(i));
+        }
+    }
+    public static void addTask(String task){
+        tasks.add(task);
+        System.out.println("added: " + task);
     }
 }
