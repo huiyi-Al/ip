294000555580cd107316756787e53a639ea7368e
Ziyue <e1295583@u.nus.edu>
2025-10-02 01:07:26 +0800

    Add support for multiple types

--- src/main/java/Greeting.java
+++ src/main/java/Greeting.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Greeting {
     private static Scanner sc = new Scanner(System.in);
     private static ArrayList<Task> tasks = new ArrayList<Task>();
+    private static int counter = 0;
+
     public static void main(String[] args) {
         greet();
 
@@ -20,8 +22,14 @@ public class Greeting {
                 markTask(instruction);
             } else if (instruction.startsWith("unmark")) {
                 unmarkTask(instruction);
+            } else if (instruction.startsWith("todo")) {
+                addTodoTask(instruction);
+            } else if (instruction.startsWith("deadline")) {
+                addDeadlineTask(instruction);
+            } else if (instruction.startsWith("event")) {
+                addEventTask(instruction);
             } else {
-                addTask(instruction);
+                System.out.println("Invalid command");
             }
         }
         exit();
@@ -43,11 +51,6 @@ public class Greeting {
             System.out.println((i + 1) + "." + tasks.get(i));
         }
     }
-    public static void addTask(String task){
-        Task newTask = new Task(task);
-        tasks.add(newTask);
-        System.out.println("added: " + task);
-    }
     public static void markTask(String task){
         String[] input = task.split(" ");
         int order = Integer.parseInt(input[1]);
@@ -62,13 +65,46 @@ public class Greeting {
         taskToUnmark.unmark();
         System.out.println("OK, I've marked this task as not done yet: \n" + taskToUnmark);
     }
+    public static void addTodoTask(String task){
+        Task newTask = new Todo(task);
+        tasks.add(newTask);
+        counter ++;
+        System.out.println("Got it. I've added this task: \n" + newTask);
+        System.out.println("Now you have " + counter + " tasks in the list");
+    }
+    public static void addDeadlineTask(String task){
+        String[] input = task.split(" /");
+        String[] Date = input[1].split(" ");
+        String[] Activity = input[0].split(" ", 2);
+        String act = Activity[1];
+        String date = Date[1];
+        Task newTask = new Deadline(act, date);
+        tasks.add(newTask);
+        counter ++;
+        System.out.println("Got it. I've added this task: \n" + newTask);
+        System.out.println("Now you have " + counter + " tasks in the list");
+    }
+    public static void addEventTask(String task){
+        String[] input = task.split(" /");
+        String[] Activity = input[0].split(" ", 2);
+        String act = Activity[1];
+        String[] time1 = input[1].split(" ", 2);
+        String start = time1[1];
+        String[] time2 = input[2].split(" ", 2);
+        String end = time2[1];
+        Task newTask = new Event(act, start, end);
+        tasks.add(newTask);
+        counter ++;
+        System.out.println("Got it. I've added this task: \n" + newTask);
+        System.out.println("Now you have " + counter + " tasks in the list");
+    }
 }
-class Task{
-    private String discription;
+abstract class Task{
+    public String description;
     private boolean isDone;
 
-    public Task(String discription) {
-        this.discription = discription;
+    public Task(String description) {
+        this.description = description;
         this.isDone = false;
     }
     public void markAsDone(){
@@ -80,8 +116,52 @@ class Task{
     public String getStatus(){
         return (isDone ? "[x]" : "[]");
     }
+    public abstract String getType();
+
     @Override
     public String toString() {
-        return getStatus() + " " + discription;
+        return "[" + getType() + "]" + getStatus() + " " + description;
+    }
+}
+class Todo extends Task{
+    public Todo(String description){
+        super(description);
+    }
+    @Override
+    public String getType() {
+        return "T";
+    }
+}
+class Deadline extends Task{
+    private String deadline;
+
+    public Deadline(String description, String deadline){
+        super(description);
+        this.deadline = deadline;
+    }
+    @Override
+    public String getType() {
+        return "D";
+    }
+    @Override
+    public String toString() {
+        return "[" + getType() + "]" + getStatus() + " " + description + " (by: " + deadline + ")";
+    }
+}
+class Event extends Task{
+    private String start;
+    private String end;
+    public Event(String description, String start, String end){
+        super(description);
+        this.start = start;
+        this.end = end;
+    }
+    @Override
+    public String getType() {
+        return "E";
+    }
+    @Override
+    public String toString() {
+        return "[" + getType() + "]" + getStatus() + " " + description + " (from: " + start + " to: " + end + ")";
     }
 }
