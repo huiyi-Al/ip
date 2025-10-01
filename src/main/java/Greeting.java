40051328950b07c22ad6a4f8f44a82444b0a3522
Ziyue <e1295583@u.nus.edu>
2025-10-01 22:25:16 +0800

    Add support for echo

--- src/main/java/Greeting.java
+++ src/main/java/Greeting.java
@@ -1,4 +1,28 @@
 package main.java;
+import java.util.Scanner;
 
 public class Greeting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        greet();
+        boolean isRunning = true;
+        while (isRunning) {
+            String instruction = sc.nextLine();
+            switch (instruction) {
+                case "bye":
+                    isRunning = false;
+                    break;
+                    default:
+                        System.out.println(instruction);
+            }
+        }
+        exit();
+    }
+    public static void greet(){
+        System.out.println("Hello! I'm Luka\n" +
+                "What can I do for you?");
+    }
+    public static void exit(){
+        System.out.println("Bye.Hope to see you again soon!");
+    }
 }
